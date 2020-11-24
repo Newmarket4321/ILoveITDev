155e2523e0a98e94a0bd9435701ebb5c0afbbc0f
kpatel <kpatel@SYSMG-09-19>
2020-11-24 15:54:56 -0500

    Update

--- TONba/Programs/ReadWriteFiles.cs
+++ TONba/Programs/ReadWriteFiles.cs
@@ -16,6 +16,7 @@ using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using DocumentFormat.OpenXml.Spreadsheet;
+using Color = System.Drawing.Color;
 
 namespace I_IT
 {
@@ -35,11 +36,11 @@ namespace I_IT
                     {
                         // Only get files that begin with the letter "pdf".
                         string[] subdirs = Directory.GetFiles(dir, "*pdf");
-                        foldername += dir + "^$";
+                        foldername += "^$" + dir;
                         
                         foreach (string subdir in subdirs)
                         {
-                            filepaths += subdir + "^$";
+                            filepaths += "^$" + subdir;
                         }
                     }
                     catch (Exception e)
@@ -52,25 +53,24 @@ namespace I_IT
                 {
                     var worksheet = workbook.Worksheets.Add("Sheet1");
 
-                    string[] splitpath = filepaths.Split(new string[] { "^$" }, StringSplitOptions.RemoveEmptyEntries);
-                    string[] splitfoldername = foldername.Split(new string[] { "^$" }, StringSplitOptions.RemoveEmptyEntries);
-                        //worksheet.Cell("A1").Value = "ISUPLOADED";
-                        //worksheet.Cell("B1").Value = "ENTITY_TYPE";
-                        //worksheet.Cell("c1").Value = "B1_PER_ID1";
-                        //worksheet.Cell("D1").Value = "B1_PER_ID2";
-                        //worksheet.Cell("E1").Value = "B1_PER_ID3";
-                        //worksheet.Cell("F1").Value = "B1_ALT_ID";
-                        //worksheet.Cell("G1").Value = "ENTITY_ID";
-                        //worksheet.Cell("H1").Value = "DOC_GROUP";
-                        //worksheet.Cell("I1").Value = "DOC_TYPE";
-                        //worksheet.Cell("J1").Value = "FILEPATH";
-                        //worksheet.Cell("K1").Value = "DESCRIPTION";
-                    
-                      for (int i= 0 ; i < splitpath.Length; i++)
-                     {
-                        string[] filesplit = splitpath[i].Split('\\');
+                    string[] splitpath = filepaths.Split(new string[] { "^$" }, StringSplitOptions.None);
+                    string[] splitfoldername = foldername.Split(new string[] { "^$" }, StringSplitOptions.None);
+                    worksheet.Cell("A1").Value = "ISUPLOADED";
+                    worksheet.Cell("B1").Value = "ENTITY_TYPE";
+                    worksheet.Cell("c1").Value = "B1_PER_ID1";
+                    worksheet.Cell("D1").Value = "B1_PER_ID2";
+                    worksheet.Cell("E1").Value = "B1_PER_ID3";
+                    worksheet.Cell("F1").Value = "B1_ALT_ID";
+                    worksheet.Cell("G1").Value = "ENTITY_ID";
+                    worksheet.Cell("H1").Value = "DOC_GROUP";
+                    worksheet.Cell("I1").Value = "DOC_TYPE";
+                    worksheet.Cell("J1").Value = "FILEPATH";
+                    worksheet.Cell("K1").Value = "DESCRIPTION";
 
-                        worksheet.Cell("A" + (i + 1)).Value = "0";
+                    for (int i= 1 ; i < splitpath.Length; i++)
+                    {
+                            string[] filesplit = splitpath[i].Split('\\');
+                            worksheet.Cell("A" + (i + 1)).Value = "0";
                             worksheet.Cell("B" + (i + 1)).Value = "CAP";
                             worksheet.Cell("c" + (i + 1)).Value = "";
                             worksheet.Cell("D" + (i + 1)).Value = "";
@@ -79,11 +79,47 @@ namespace I_IT
                             worksheet.Cell("G" + (i + 1)).Value = "";
                             worksheet.Cell("H" + (i + 1)).Value = "";
                             worksheet.Cell("I" + (i + 1)).Value = "";
-                            worksheet.Cell("J" + (i + 1)).Value = splitpath[i];
+                            worksheet.Cell("J" + (i + 1)).Value = splitpath[i].Replace("T:\\DI Services\\Building\\Plans Examination\\00-APPLICATION DRAWINGS\\", "AccelaDocs\\"); 
                             worksheet.Cell("K" + (i + 1)).Value = "";
 
-                      }
+                    }
+                    worksheet.Cell("A1").Style.Alignment.WrapText = true;
+                    worksheet.Cell("A1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    
+                    worksheet.Cell("B1").Style.Alignment.WrapText = true;
+                    worksheet.Cell("B1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
 
+                    worksheet.Cell("C1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    worksheet.Cell("C1").Style.Alignment.WrapText = true;
+
+                    worksheet.Cell("D1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    worksheet.Cell("D1").Style.Alignment.WrapText = true;
+
+                    worksheet.Cell("E1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    worksheet.Cell("E1").Style.Alignment.WrapText = true;
+
+                    worksheet.Cell("F1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    worksheet.Cell("F1").Style.Alignment.WrapText = true;
+
+                    worksheet.Cell("G1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    worksheet.Cell("G1").Style.Alignment.WrapText = true;
+
+                    worksheet.Cell("H1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    worksheet.Cell("H1").Style.Alignment.WrapText = true;
+
+                    worksheet.Cell("I1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    worksheet.Cell("I1").Style.Alignment.WrapText = true;
+
+                    worksheet.Cell("J1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    worksheet.Cell("J1").Style.Alignment.WrapText = true;
+
+                    worksheet.Cell("K1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    worksheet.Cell("K1").Style.Alignment.WrapText = true;
+
+                    worksheet.Columns("A").Width = 4;
+                    worksheet.Columns("B").Width = 4;
+                    worksheet.Columns("F").Width = 52;
+                    worksheet.Columns("J").Width = 120;
                     workbook.SaveAs(@"C:\Users\"+ Environment.UserName + @"\00-APPLICATION DRAWINGS.xlsx");
                 }
                 string[] dirs1 = Directory.GetDirectories(@"T:\DI Services\Building\Plans Examination\1- Issued Permits\");
@@ -95,11 +131,11 @@ namespace I_IT
                     {
                         // Only get files that begin with the letter "pdf".
                         string[] subdirs1 = Directory.GetFiles(dir1, "*pdf");
-                        foldername1 += dir1 + "^$";
+                        foldername1 += "^$" + dir1 ;
 
                         foreach (string subdir1 in subdirs1)
                         {
-                            filepaths1 += subdir1 + "^$";
+                            filepaths1 += "^$" + subdir1 ;
                         }
                     }
                     catch (Exception e)
@@ -111,13 +147,23 @@ namespace I_IT
                 using (var workbook1 = new XLWorkbook())
                 {
                     var worksheet1 = workbook1.Worksheets.Add("Sheet1");
-                    
-                    string[] splitpath1 = filepaths1.Split(new string[] { "^$" }, StringSplitOptions.RemoveEmptyEntries);
-                    string[] splitfoldername1 = foldername1.Split(new string[] { "^$" }, StringSplitOptions.RemoveEmptyEntries);
-
-                    for (int i = 0; i < splitpath1.Length; i++)
+                    string[] splitpath1 = filepaths1.Split(new string[] { "^$" }, StringSplitOptions.None);
+                    string[] splitfoldername1 = foldername1.Split(new string[] { "^$" }, StringSplitOptions.None);
+                    worksheet1.Cell("A1").Value = "ISUPLOADED";
+                    worksheet1.Cell("B1").Value = "ENTITY_TYPE";
+                    worksheet1.Cell("c1").Value = "B1_PER_ID1";
+                    worksheet1.Cell("D1").Value = "B1_PER_ID2";
+                    worksheet1.Cell("E1").Value = "B1_PER_ID3";
+                    worksheet1.Cell("F1").Value = "B1_ALT_ID";
+                    worksheet1.Cell("G1").Value = "ENTITY_ID";
+                    worksheet1.Cell("H1").Value = "DOC_GROUP";
+                    worksheet1.Cell("I1").Value = "DOC_TYPE";
+                    worksheet1.Cell("J1").Value = "FILEPATH";
+                    worksheet1.Cell("K1").Value = "DESCRIPTION";
+                    for (int i = 1; i < splitpath1.Length; i++)
                     {
                         string[] filesplit1 = splitpath1[i].Split('\\');
+                        //T:\DI Services\Building\Plans Examination\00-APPLICATION DRAWINGS
 
                         worksheet1.Cell("A" + (i + 1)).Value = "0";
                         worksheet1.Cell("B" + (i + 1)).Value = "CAP";
@@ -128,11 +174,48 @@ namespace I_IT
                         worksheet1.Cell("G" + (i + 1)).Value = "";
                         worksheet1.Cell("H" + (i + 1)).Value = "";
                         worksheet1.Cell("I" + (i + 1)).Value = "";
-                        worksheet1.Cell("J" + (i + 1)).Value = splitpath1[i];
+                        worksheet1.Cell("J" + (i + 1)).Value = splitpath1[i].Replace("T:\\DI Services\\Building\\Plans Examination\\1- Issued Permits\\", "AccelaDocs\\");
+                        
                         worksheet1.Cell("K" + (i + 1)).Value = "";
 
                     }
+                    
+                    worksheet1.Cell("A1").Style.Alignment.WrapText = true;
+                    worksheet1.Cell("A1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    
+                    worksheet1.Cell("B1").Style.Alignment.WrapText = true;
+                    worksheet1.Cell("B1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
 
+                    worksheet1.Cell("C1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    worksheet1.Cell("C1").Style.Alignment.WrapText = true;
+
+                    worksheet1.Cell("D1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    worksheet1.Cell("D1").Style.Alignment.WrapText = true;
+
+                    worksheet1.Cell("E1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    worksheet1.Cell("E1").Style.Alignment.WrapText = true;
+
+                    worksheet1.Cell("F1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    worksheet1.Cell("F1").Style.Alignment.WrapText = true;
+
+                    worksheet1.Cell("G1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    worksheet1.Cell("G1").Style.Alignment.WrapText = true;
+
+                    worksheet1.Cell("H1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    worksheet1.Cell("H1").Style.Alignment.WrapText = true;
+
+                    worksheet1.Cell("I1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    worksheet1.Cell("I1").Style.Alignment.WrapText = true;
+
+                    worksheet1.Cell("J1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    worksheet1.Cell("J1").Style.Alignment.WrapText = true;
+
+                    worksheet1.Cell("K1").Style.Fill.BackgroundColor = XLColor.PeachOrange;
+                    worksheet1.Cell("K1").Style.Alignment.WrapText = true;
+                    worksheet1.Columns("A").Width = 4;
+                    worksheet1.Columns("B").Width = 4;
+                    worksheet1.Columns("F" ).Width = 52;
+                    worksheet1.Columns("J").Width = 120;
                     workbook1.SaveAs(@"C:\Users\" + Environment.UserName + @"\1- Issued Permits.xlsx");
                     MessageBox.Show(@"Your file is saved in C:\Users\"+Environment.UserName);
                 }
